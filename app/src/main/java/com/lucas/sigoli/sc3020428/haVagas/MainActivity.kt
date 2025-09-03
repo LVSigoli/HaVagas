75c9a8fb6a8cb9f7396c1fbab38d5a5d9ca215a5
LVSigoli <lucasviniciussigoli@gmail.com>
2025-09-03 20:58:47 -0300

    feat: Implement onCreate and auxiliar function

--- app/src/main/java/com/lucas/sigoli/sc3020428/haVagas/MainActivity.kt
+++ app/src/main/java/com/lucas/sigoli/sc3020428/haVagas/MainActivity.kt
@@ -1,21 +1,45 @@
 package com.lucas.sigoli.sc3020428.haVagas
 
+
+
+
 import android.os.Bundle
+import android.view.View
+
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import  com.lucas.sigoli.sc3020428.haVagas.MainActivity
+import com.lucas.sigoli.sc3020428.haVagas.databinding.ActivityMainBinding
+
+
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+
+private lateinit var binding: ActivityMainBinding
+    private var educationExtraView: MutableList<View> = mutableListOf()
+
+    override fun onCreate(savedInstance: Bundle?) {
+
+        super.onCreate(savedInstance)
+
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.etNome)) { v, insets ->
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        applyInsets()
+
+    }
+
+    private fun applyInsets() {
+        ViewCompat.setOnApplyWindowInsetsListener(binding.etNome) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
+
+
 }
